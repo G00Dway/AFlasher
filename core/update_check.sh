a88fc6ac48ae840d08c567028c9168cc61173b7b
G00Dway <80381071+G00Dway@users.noreply.github.com>
2021-07-11 11:27:21 +0400

    Add files via upload

---
+++ core/update_check.sh
@@ -0,0 +1,19 @@
+echo -e "\033[1;34m[*] \033[0mChecking For Updates..."
+BRANCH="main"
+LAST_UPDATE=`git show --no-notes --format=format:"%H" $BRANCH | head -n 1`
+LAST_COMMIT=`git show --no-notes --format=format:"%H" origin/$BRANCH | head -n 1`
+
+git remote update > /dev/null 2>&1
+if [ $LAST_COMMIT != $LAST_UPDATE ]; then
+        echo -e "\033[1;34m[*] \033[0mUpdate Available! Updating..."
+        git pull --no-edit > /dev/null 2>&1
+        echo -e "\033[1;32m[+] \033[0mUpdate Succesfull!"
+else
+        echo -e "\033[1;32m[+] \033[0mNo updates Available!"
+fi
+#\033[1;77m[i] \033[0m
+#\033[1;77m[?] \033[0m
+#\033[1;32m[+] \033[0m
+#\033[1;33m[!] \033[0m
+#\033[1;31m[-] \033[0m
+#\033[1;34m[*] \033[0m
